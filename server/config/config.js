a5fb43fdd928410cbaefacc101bc7739d143b7e2
Sebastian Valencia <sebastian.valencian@gmail.com>
2020-02-23 18:41:17 -0500

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 //Base de datos
 let urlDB;
-// if (process.env.NODE_ENV === "dev") {
-//   urlDB = "mongodb://localhost:27017/cafe";
-// } else {
-  urlDB =
-    "mongodb+srv://sebas:pvj8NTUqdMvsdUxO@cluster0-kivrn.mongodb.net/cafe";
-// }
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cafe";
+} else {
+//   urlDB = "mongodb+srv://sebas:pvj8NTUqdMvsdUxO@cluster0-kivrn.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
